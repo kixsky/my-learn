1d79a8b85dbe76eac6bca943147a75f168bf96e5
moshenglei <moshenglei@zhongan.com>
2020-01-13 14:17:45 +0800

    b

--- hello-netty/src/main/java/com/mosl/netty/TimeServer.java
+++ hello-netty/src/main/java/com/mosl/netty/TimeServer.java
@@ -1,4 +1,13 @@
 package com.mosl.netty;
 
 public class TimeServer {
+
+    public void bing(int port) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        int port = 8080;
+
+    }
 }
---
+++ hello-netty/src/main/java/com/mosl/netty/TimeServerHandler.java
@@ -0,0 +1,32 @@
+package com.mosl.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.Date;
+
+public class TimeServerHandler extends ChannelHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf)msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8");
+        System.out.println("The time server receive order : " + body);
+//        String currentTime = "".equalsIgnoreCase(body) ? new Date()
+    }
+
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+}
